2569ee17f298fee34b8eaad6bcd34887de42b2a5
user2 <user2@DESKTOP-FTCMOTQ>
2020-03-23 20:50:18 +0900

    OOPExercise_BaekJongHyun Init
    OOPExercise_BaekJongHyun 작성과 github 연습을 위해 commit & push 합니다.

---
+++ src/member/run/Main.java
@@ -0,0 +1,10 @@
+package member.run;
+
+//import member.view.MemberMenu;
+
+public class Main {
+	public static void main(String[] args) {
+//		MemberMenu mMenu = new MemberMenu();		
+//		mMenu.mainMenu();
+	}
+}
